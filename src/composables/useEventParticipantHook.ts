f954cd55859086ff8880220827a3b5db0a0b4a36
unknown <jgpschianti07@gmail.com>
2024-08-22 09:04:45 -0300

    creating api

---
+++ src/composables/useEventParticipantHook.ts
@@ -0,0 +1,123 @@
+import { eventParticipantApi } from "@/services/api";
+
+
+function useEventParticipantHook() {
+  const eventParticipantControllerFindAllPublicEvents = async ( 
+    perPage: string,
+    page : string,
+    title?: string,
+    category?: string
+  ) => {
+    try {
+      const { data, status, statusText } =
+        await eventParticipantApi.eventParticipantControllerFindAllPublicEvents(
+          page,
+          perPage,
+          title,
+          category
+        );
+      return {
+        status: status,
+        message: statusText,
+        data: data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.response.data.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  };
+
+  const eventParticipantControllerFindOnePublicEvent = async (slug: string) => {
+    try {
+      const { data, status, statusText } =
+        await eventParticipantApi.eventParticipantControllerFindOnePublicEvent(
+          slug
+        );
+      return {
+        status: status,
+        message: statusText,
+        data: data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.response.data.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  };
+
+  
+ {
+    let a = "a"
+ }
+  const eventParticipantControllerGetEventsMoreView = async () => {
+    try {
+      const { data, status, statusText } =
+        await eventParticipantApi.eventParticipantControllerGetEventsMoreView();
+      return {
+        status: status,
+        message: statusText,
+        data: data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.response.data.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  };
+
+  const eventParticipantControllerFindAllPublicEventsHome = async () => {
+    try {
+      const { data, status, statusText } =
+        await eventParticipantApi.eventParticipantControllerFindAllPublicEventsHome();
+      return {
+        status: status,
+        message: statusText,
+        data: data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.response.data.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  };
+
+  
+
+  const eventParticipantControllerEventAddViewCount = async (
+    eventSlug: string
+  ) => {
+    try {
+      const { data, status, statusText } =
+        await eventParticipantApi.eventParticipantControllerEventAddViewCount(
+          eventSlug
+        );
+      return {
+        status: status,
+        message: statusText,
+        data: data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.response.data.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  };
+
+
+
+
+  return {
+    eventParticipantControllerFindAllPublicEvents,
+    eventParticipantControllerFindOnePublicEvent,
+    eventParticipantControllerGetEventsMoreView,
+    eventParticipantControllerFindAllPublicEventsHome,
+    eventParticipantControllerEventAddViewCount,
+  };
+}
+export default useEventParticipantHook;
